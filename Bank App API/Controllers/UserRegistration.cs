da30d18accab3fccfa77fb0e1d9a5234d9500c7f
ASUS <ASUS@LAPTOP-KUUEHDR0>
2025-07-07 22:10:14 +0530

    Bank-app-exception-handling

--- Bank App API/Controllers/UserRegistration.cs
+++ Bank App API/Controllers/UserRegistration.cs
@@ -24,56 +24,70 @@ namespace Bank_App_API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> RegisterUser(User user)
         {
-            if (user == null)
-                return BadRequest("User data is null");
-            //if (!ModelState.IsValid)
-            //    return BadRequest(ModelState);
-            var existingUser = await _userDb.Users.FirstOrDefaultAsync(u => u.CustomerId == user.CustomerId);
-            if (existingUser != null)
-                return Conflict("User with this Customer ID already exists");
-            var hasher = new PasswordHasher<User>();
-            user.Password = hasher.HashPassword(user, user.Password);
-            await _userDb.Users.AddAsync(user);
-            await _userDb.SaveChangesAsync();
-            return Ok("user registered sucessfully");
-            //return CreatedAtAction(nameof(RegisterUser), new { id = user.CustomerId }, user);
+            try
+            {
+                if (user == null)
+                    return BadRequest("User data is null");
+                //if (!ModelState.IsValid)
+                //    return BadRequest(ModelState);
+                var existingUser = await _userDb.Users.FirstOrDefaultAsync(u => u.CustomerId == user.CustomerId);
+                if (existingUser != null)
+                    return Conflict("User with this Customer ID already exists");
+                var hasher = new PasswordHasher<User>();
+                user.Password = hasher.HashPassword(user, user.Password);
+                await _userDb.Users.AddAsync(user);
+                await _userDb.SaveChangesAsync();
+                return Ok("user registered sucessfully");
+                //return CreatedAtAction(nameof(RegisterUser), new { id = user.CustomerId }, user);
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> login(string username, int customerId, string password)
         {
-            var result = await _userDb.Users.FirstOrDefaultAsync(u => u.Name.ToLower().Trim() == username.ToLower().Trim() && u.CustomerId == customerId);
-            if (result == null)
-                return Unauthorized("Invalid credentials");
-
-            var hasher = new PasswordHasher<User>();
-            var verificationResult = hasher.VerifyHashedPassword(result, result.Password, password);
-
-            if (verificationResult == PasswordVerificationResult.Success)
+            try
             {
-                Claim[] claims = new Claim[]
+                var result = await _userDb.Users.FirstOrDefaultAsync(u => u.Name.ToLower().Trim() == username.ToLower().Trim() && u.CustomerId == customerId);
+                if (result == null)
+                    return Unauthorized("Invalid credentials");
+
+                var hasher = new PasswordHasher<User>();
+                var verificationResult = hasher.VerifyHashedPassword(result, result.Password, password);
+
+                if (verificationResult == PasswordVerificationResult.Success)
                 {
+                    Claim[] claims = new Claim[]
+                    {
                 new Claim(ClaimTypes.Name, result.Name),
                 new Claim(ClaimTypes.Role, result.IsAdmin ? "Admin" : "User"),
                 new Claim("CustomerId", result.CustomerId.ToString())
-                };
+                    };
 
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]));
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]));
+                    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-                var token = new JwtSecurityToken(
-                    issuer: _config["JwtSettings:Issuer"],
-                    audience: _config["JwtSettings:Audience"],
-                    claims: claims,
-                    expires: DateTime.Now.AddMinutes(30),
-                    signingCredentials: creds);
+                    var token = new JwtSecurityToken(
+                        issuer: _config["JwtSettings:Issuer"],
+                        audience: _config["JwtSettings:Audience"],
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(30),
+                        signingCredentials: creds);
 
-                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+                    return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+                }
+                else
+                {
+                    return Unauthorized("Invalid credentials");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return Unauthorized("Invalid credentials");
+                throw ex;
             }
+
         }
     }
 }
